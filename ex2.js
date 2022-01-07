f3a4207d35381003335550449dd50d52311aba7e
Andrei Popescu <andrei-popescu@outlook.com>
2022-01-07 12:37:05 +0100

    Initial commit

---
+++ ex2.js
@@ -0,0 +1,7 @@
+/*Parse your solution of exercise 1.1 in JavaScript using the library jsonld.js and
+
+Print an RDF representation of the JSON-LD data.
+Extract organization information using a frame.
+*/
+
+const jsonld = require('jsonld');
