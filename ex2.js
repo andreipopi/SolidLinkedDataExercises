884a2eeeb01f6db871b8c34eda2ba635c1279547
Andrei Popescu <andrei-popescu@outlook.com>
2022-01-07 13:46:54 +0100

    added ex2

--- ex2.js
+++ ex2.js
@@ -3,5 +3,32 @@
 Print an RDF representation of the JSON-LD data.
 Extract organization information using a frame.
 */
+import jsonld from 'jsonld';
 
-const jsonld = require('jsonld');
+const context = {
+    "foaf": "http://xmlns.com/foaf/0.1/",
+    "org": "http://www.w3.org/ns/org"
+};
+
+const doc = {
+    "@id": "https://new-u.tech/gwen/#me",
+    "@type": "foaf:Person",
+
+    "foaf:name": "Gwendolyne",
+    "foaf:lastName": "Stacy",
+    "foaf:nick": "Gwen",
+    "foaf:homepage": "https://new-u.tech/gwen",
+    "foaf:email": "gwen@new-u.tech",
+    "foaf:img": "image",
+    "org:memberOf": {
+        "@type": "org:Organization",
+        "foaf:name": "New U Technologies",
+        "foaf:homepage": "https://new-u.tech"
+    }
+};
+
+
+
+const nquads =  await jsonld.toRDF(doc, {format: 'application/n-quads'});
+
+console.log(nquads);
